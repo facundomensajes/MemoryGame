a7edd4a259a066a3ebaa3ee36d078f00a89c5ca1
facundomensajes <facundomensajes@gmail.com>
2018-02-25 17:20:57 -0300

    firs commit

---
+++ Assets/MemoryCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+
+	[SerializeField] private GameObject cardBack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnMouseDown() 
+	{
+		if (cardBack.activeSelf) {
+			cardBack.SetActive(false);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
